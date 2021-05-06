29d1af8ddcf7d198e90bd9eef0577baeec087bdf
Andy Million <me@aaron-powell.com>
2021-05-06 09:24:00 -0500

    Added NavBar and EventCard component. adjusted routing as needed

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,16 +1,18 @@
 <template>
   <div>
-      <h1>Events Listing</h1>
-      <router-link :to="{ name: 'event-show', params: {id: '1'}}">Show Event #1</router-link>
+    <h1>Events Listing</h1>
+    <EventCard/>
   </div>
 </template>
 
 <script>
+import EventCard from "../components/EventCard.vue";
+
 export default {
-    name: 'EventList'
-}
+  components: {
+    EventCard,
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
